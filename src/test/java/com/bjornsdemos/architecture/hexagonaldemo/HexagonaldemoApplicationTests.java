782c0d8106d8be381439f35fb59aa3f55cee972b
bjorn <bjorn.decraemer@yahoo.com>
2021-12-02 12:28:36 +0100

    Initial commit

---
+++ src/test/java/com/bjornsdemos/architecture/hexagonaldemo/HexagonaldemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bjornsdemos.architecture.hexagonaldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonaldemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
